2eae6bc93ebce6e9223fd9c281fbe03835567466
Oriol Canadés <oriol.canades@in2.es>
2024-08-20 10:08:22 +0200

    New main() test Update Sonar configurations

--- src/test/java/es/in2/vcverifier/VcVerifierApplicationTests.java
+++ src/test/java/es/in2/vcverifier/VcVerifierApplicationTests.java
@@ -1,5 +1,6 @@
 package es.in2.vcverifier;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +10,12 @@ class VcVerifierApplicationTests {
     @Test
     void contextLoads() {
         // The test will automatically fail if the application context cannot be loaded.
+    }
+
+    @Test
+    void testMain() {
+        VcVerifierApplication.main(new String[]{});
+        Assertions.assertTrue(true);
     }
 
 }
