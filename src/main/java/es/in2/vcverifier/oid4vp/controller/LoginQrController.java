0a6278516c289ae396a53250c575fc3d32547c00
javieralvarezin2 <javier.alvarez@in2.es>
2024-09-25 17:08:00 +0200

    refactor Controller to RestController

--- src/main/java/es/in2/vcverifier/oid4vp/controller/LoginQrController.java
+++ src/main/java/es/in2/vcverifier/oid4vp/controller/LoginQrController.java
@@ -5,15 +5,12 @@ import net.glxn.qrgen.javase.QRCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
 
-@Controller
+@RestController
 public class LoginQrController {
 
 
--- src/main/java/es/in2/vcverifier/security/JWKSourceCreationException.java
+++
@@ -1,9 +0,0 @@
-package es.in2.vcverifier.security;
-
-public class JWKSourceCreationException extends RuntimeException {
-
-    public JWKSourceCreationException(String message) {
-        super(message);
-    }
-
-}
