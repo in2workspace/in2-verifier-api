e79d29193f01ba599db00f573356111d90065233
RubenModamioGarcia <ruben.modamio@in2.es>
2024-09-25 15:00:59 +0200

    add exception for the CustomExceptionHandler to manage cache retrieval

--- src/main/java/es/in2/vcverifier/exception/handler/CustomExceptionHandler.java
+++ src/main/java/es/in2/vcverifier/exception/handler/CustomExceptionHandler.java
@@ -6,11 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String>handleNoSuchElementException(NoSuchElementException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
