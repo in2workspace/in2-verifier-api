d661296a13930ec3a120ba2a8a47d5aa92939bc6
RubenModamioGarcia <ruben.modamio@in2.es>
2024-10-30 12:05:48 +0100

    rename property

--- src/main/java/es/in2/vcverifier/model/ClientData.java
+++ src/main/java/es/in2/vcverifier/model/ClientData.java
@@ -1,7 +1,10 @@
 package es.in2.vcverifier.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ClientData(
         String id,
         String url,
