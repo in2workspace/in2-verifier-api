c0f9b190b3ead9f4b516c5c3bb54ee9e4962d8f2
Dmitri Sidorov Manko <159002112+dmitriin2@users.noreply.github.com>
2024-11-20 13:08:56 +0100

    Fix tests

--- src/test/java/es/in2/vcverifier/security/filters/CustomTokenRequestConverterTest.java
+++ src/test/java/es/in2/vcverifier/security/filters/CustomTokenRequestConverterTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jwt.SignedJWT;
 import es.in2.vcverifier.config.CacheStore;
 import es.in2.vcverifier.exception.InvalidCredentialTypeException;
+import es.in2.vcverifier.exception.InvalidVPtokenException;
 import es.in2.vcverifier.exception.UnsupportedGrantTypeException;
 import es.in2.vcverifier.model.AuthorizationCodeData;
 import es.in2.vcverifier.model.credentials.machine.LEARCredentialMachine;
@@ -192,7 +193,7 @@ class CustomTokenRequestConverterTest {
         when(clientAssertionValidationService.validateClientAssertionJWTClaims(anyString(), any())).thenReturn(true);
         when(vpService.validateVerifiablePresentation(anyString())).thenReturn(false);
 
-        assertThrows(IllegalArgumentException.class, () ->
+        assertThrows(InvalidVPtokenException.class, () ->
                 customTokenRequestConverter.convert(mockRequest));
     }
 
