e77c6d94792a41874e7df23fc0785a49929e0d99
Oriol Canadés <oriol.canades@in2.es>
2024-12-04 12:34:10 +0100

    Update Mandate.java

--- src/main/java/es/in2/verifier/model/credentials/lear/Mandate.java
+++ src/main/java/es/in2/verifier/model/credentials/lear/Mandate.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 import java.util.List;
+
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
