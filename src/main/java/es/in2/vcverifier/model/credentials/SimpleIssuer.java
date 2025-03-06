e6d9a584622fcbcec9315f92abb5887c08900765
rubenmodamioin2 <145454887+rubenmodamioin2@users.noreply.github.com>
2025-03-06 15:34:38 +0100

    solve problem with issuer serialization (#38)

--- src/main/java/es/in2/vcverifier/model/credentials/SimpleIssuer.java
+++ src/main/java/es/in2/vcverifier/model/credentials/SimpleIssuer.java
@@ -2,11 +2,18 @@ package es.in2.vcverifier.model.credentials;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Builder;
 
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SimpleIssuer(@JsonProperty("id") String id) implements Issuer {
+
+    @JsonValue
+    public String toJson() {
+        return id;
+    }
+
     @Override
     public String getId() {
         return id;
